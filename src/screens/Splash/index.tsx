7f927c336ae4eda7dc65cb350b864672d50e0c97
JUOMATSU <julianaomatsup@gmail.com>
2025-07-01 20:08:19 -0300

    Adiciona opacidade

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,15 +1,28 @@
-import { View, Text } from "react-native";
+import React { useEffect, useRef } from "react";
+import { Animated, View, Text } from "react-native";
 import LottieView from "lottie-react-native";
 import { styles } from "./style";
 export default function Splash() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1500,
+      usenativeDriver: true,
+    }).start();
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Animated.View style={{ opacity: fadeAnim }}>
       <LottieView
         source={require("../../assets/splash.json")}
         style={styles.splash}
         autoPlay
         loop
       />
+    </Animated.View>
     </View>
   );
 }
